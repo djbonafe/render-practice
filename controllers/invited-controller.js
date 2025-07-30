eb9d76cc4fcbb637ae590e86f16c975369938de5
djbonafe <Dominic.Jisum.S.Bonafe@gds.ey.com>
2025-07-31 00:49:41 +0800

    fixed specialRequest field to match with frontend

--- controllers/invited-controller.js
+++ controllers/invited-controller.js
@@ -1,10 +1,8 @@
-const {validationResult} = require('express-validator'); 
+const { validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-
-const Invited = require('../models/invited'); 
+const Invited = require('../models/invited');
 const HttpError = require('../models/http-error');
-const { ValidatorsImpl } = require('express-validator/lib/chain');
 
 const getAllInvited = async (req, res, next) => {
   let allInvited;
@@ -22,59 +20,50 @@ const getAllInvited = async (req, res, next) => {
   });
 };
 
-
 const getInvitedById = async (req, res, next) => {
-    const invId = req.params.invId; 
+  const invId = req.params.invId;
 
-    let invited; 
-    try{
-    invited =  await Invited.findById(invId)
-    }catch(err){
-        const error = new HttpError('Something went wrong, could not find the invitee', 500)
-     return next(error)
-    }
+  let invited;
+  try {
+    invited = await Invited.findById(invId);
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not find the invitee', 500);
+    return next(error);
+  }
 
-    if (!invited) {
-    const error = new HttpError(
-      'Could not find invited for the provided id.',
-      404
-    );
+  if (!invited) {
+    const error = new HttpError('Could not find invited for the provided id.', 404);
     return next(error);
   }
 
   res.json({ invited: invited.toObject({ getters: true }) });
-
-}
+};
 
 const createInvited = async (req, res, next) => {
-    const errors = validationResult(req); 
-if(!errors.isEmpty()) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     const errorMessages = errors.array().map(err => err.msg).join(', ');
- 
-    return next(
-      new HttpError(`Invalid inputs: ${errorMessages}`, 422)
-      
-    );
+    return next(new HttpError(`Invalid inputs: ${errorMessages}`, 422));
   }
-    const {firstName, lastName, coming, specialRequests} = req.body;
-    
-    const createdInvited = new Invited({
-        firstName, 
-        lastName, 
-        coming, 
-        specialRequests
-    })
 
-    try{
-        await createdInvited.save(); 
-        
-    }catch (err) {
-  console.error('[Invited Save Error]:', err);
-  return next(new HttpError('Creating invited failed, please try again.', 500));
-}
-    res.status(201).json({invited: createdInvited})
+  const { firstName, lastName, coming, specialRequest } = req.body;
 
-}
+  const createdInvited = new Invited({
+    firstName,
+    lastName,
+    coming,
+    specialRequest
+  });
+
+  try {
+    await createdInvited.save();
+  } catch (err) {
+    console.error('[Invited Save Error]:', err);
+    return next(new HttpError('Creating invited failed, please try again.', 500));
+  }
+
+  res.status(201).json({ invited: createdInvited });
+};
 
 const deleteInvited = async (req, res, next) => {
   const invId = req.params.invId;
@@ -88,16 +77,12 @@ const deleteInvited = async (req, res, next) => {
   }
 
   if (!invited) {
-    const error = new HttpError(
-      'Could not find invited for the provided id.',
-      404
-    );
+    const error = new HttpError('Could not find invited for the provided id.', 404);
     return next(error);
   }
-res.status(200).json({ message: 'Invited deleted.' });
 
+  res.status(200).json({ message: 'Invited deleted.' });
 };
-
 
 const editInvited = async (req, res, next) => {
   const errors = validationResult(req);
@@ -107,7 +92,7 @@ const editInvited = async (req, res, next) => {
   }
 
   const invId = req.params.invId;
-  const { firstName, lastName, coming, specialRequests } = req.body;
+  const { firstName, lastName, coming, specialRequest } = req.body;
 
   let updatedInvited;
   try {
@@ -117,7 +102,7 @@ const editInvited = async (req, res, next) => {
         firstName,
         lastName,
         coming,
-        specialRequests
+        specialRequest
       },
       { new: true, runValidators: true }
     );
@@ -126,17 +111,15 @@ const editInvited = async (req, res, next) => {
       return next(new HttpError('Invitee not found.', 404));
     }
 
-    res.status(200).json({ message: 'Invitee updated.', invited: updatedInvited }); // ✅ THIS PART
+    res.status(200).json({ message: 'Invited updated.', invited: updatedInvited });
   } catch (err) {
     console.error('[Update Invited Error]:', err);
     return next(new HttpError('Updating invited failed, please try again.', 500));
   }
 };
 
-
-
-exports.createInvited = createInvited; 
-exports.getInvitedById = getInvitedById; 
-exports.getAllInvited = getAllInvited; 
-exports.deleteInvited = deleteInvited; 
+exports.createInvited = createInvited;
+exports.getInvitedById = getInvitedById;
+exports.getAllInvited = getAllInvited;
+exports.deleteInvited = deleteInvited;
 exports.editInvited = editInvited;
